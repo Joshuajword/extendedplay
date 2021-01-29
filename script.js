4d4bb1bccb30f18f4a36281674a2657aff8e4112
Joshuajword <joshuajballardword@gmail.com>
2021-01-28 19:36:23 -0500

    JS code for Seat Geek is not forcing errors but still needs to call

--- script.js
+++ script.js
@@ -29,31 +29,44 @@ searchButton.on("click", function(){
     console.log(bandInput.val(), locationInput.val());
     localBreweries();
     bandInformation();
+    concertInformation();
 })
+
+function concertInformation(){
+    var queryUrl = "https://api.seatgeek.com/2/events?client_id=MjE1MTc5MTV8MTYxMTcwODQ0MS43NTExMTk0&client_secret=f4b171fe10abb7596219cdc85cc92ea099eed88a2f981f277950a5325b27cfe6" + locationInput.val();
+    var queryUrlBand = "https://api.seatgeek.com/2/performers/?client_id=MjE1MTc5MTV8MTYxMTcwODQ0MS43NTExMTk0&client_secret=f4b171fe10abb7596219cdc85cc92ea099eed88a2f981f277950a5325b27cfe6" + bandInput.val();
+
+    $.ajax ({
+        url:queryUrl,
+        method:"Get",
+    })
+    .then(function(concerts) {
+        console.log(queryUrl);
+        console.log(queryUrlBand);
+    })
+}
 
 
 function localBreweries(){
-var  queryUrl = "https://api.openbrewerydb.org/breweries?by_postal=" + locationInput.val();
+    var  queryUrl = "https://api.openbrewerydb.org/breweries?by_postal=" + locationInput.val();
 
-$.ajax ({
-    url:queryUrl,
-    method:"Get",
+    $.ajax ({
+        url:queryUrl,
+        method:"Get",
 }) 
-.then(function(breweryList) {
-    console.log(queryUrl);
-    console.log(breweryList);
-    `<a href="-">link to breweries</a>`
-    breweries.empty()
-    for (var i = 0; i < breweryList.length; i++){
-        breweries.append(`<a href="${breweryList[i].website_url}">${breweryList[i].name}</a>`)
-    }
-})  
-
-
+    .then(function(breweryList) {
+        console.log(queryUrl);
+        console.log(breweryList);
+        `<a href="-">link to breweries</a>`
+        breweries.empty()
+        for (var i = 0; i < breweryList.length; i++){
+            breweries.append(`<a href="${breweryList[i].website_url}">${breweryList[i].name}</a>`)
+        }
+    })  
 }
 
 function bandInformation(){
-    var queryUrlBand = "https://api.discogs.com/artists/1?callback=" + bandInput.val();
+    var queryUrlBand = "https://api.discogs.com/artists/?callback=" + bandInput.val();
     $.ajax ({
         url:queryUrlBand,
         method:"Get",
